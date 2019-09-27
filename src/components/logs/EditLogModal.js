76ab1a08050f87eb44eb056dcb41d0620551e446
Sasa Radovanovic <salle90ar@gmail.com>
2019-09-27 23:54:29 +0200

    Update Logs

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { updateLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
+
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message);
+      setAttention(current.attention);
+      setTech(current.tech);
+    }
+  }, [current]);
 
   const onSubmit = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log(message, tech, attention);
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+
+      updateLog(updLog);
+      M.toast({ html: `Log updated by ${tech}` });
 
       //   Clear Fields
       setMessage('');
@@ -20,46 +40,43 @@ const EditLogModal = () => {
   };
 
   return (
-    <div id="edit-log-modal" className="modal" style={modalStyle}>
-      <div className="modal-content">
+    <div id='edit-log-modal' className='modal' style={modalStyle}>
+      <div className='modal-content'>
         <h4>Enter System Log</h4>
-        <div className="row">
-          <div className="input-field">
+        <div className='row'>
+          <div className='input-field'>
             <input
-              type="text"
-              name="message"
+              type='text'
+              name='message'
               value={message}
               onChange={e => setMessage(e.target.value)}
             />
-            <label htmlFor="message" className="active">
-              Log Message
-            </label>
           </div>
         </div>
-        <div className="row">
-          <div className="input-field">
+        <div className='row'>
+          <div className='input-field'>
             <select
-              name="tech"
+              name='tech'
               value={tech}
-              className="browser-default"
+              className='browser-default'
               onChange={e => setTech(e.target.value)}
             >
-              <option value="" disabled>
+              <option value='' disabled>
                 Select Technician
               </option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <option value='John Doe'>John Doe</option>
+              <option value='Sam Smith'>Sam Smith</option>
+              <option value='Sara Wilson'>Sara Wilson</option>
             </select>
           </div>
         </div>
-        <div className="row">
-          <div className="input-field">
+        <div className='row'>
+          <div className='input-field'>
             <p>
               <label>
                 <input
-                  type="checkbox"
-                  className="filled-in"
+                  type='checkbox'
+                  className='filled-in'
                   checked={attention}
                   value={attention}
                   onChange={e => setAttention(!attention)}
@@ -70,11 +87,11 @@ const EditLogModal = () => {
           </div>
         </div>
       </div>
-      <div className="modal-footer">
+      <div className='modal-footer'>
         <a
-          href="#!"
+          href='#!'
           onClick={onSubmit}
-          className="modal-close waves-effect waves-light blue btn"
+          className='modal-close waves-effect waves-light blue btn'
         >
           Enter
         </a>
@@ -88,4 +105,16 @@ const modalStyle = {
   height: '75%'
 };
 
-export default EditLogModal;
+EditLogModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  current: state.log.current
+});
+
+export default connect(
+  mapStateToProps,
+  { updateLog }
+)(EditLogModal);
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import Moment from 'react-moment';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const LogItem = ({ log, deleteLog, setCurrent }) => {
+  const onDelete = () => {
+    deleteLog(log.id);
+    M.toast({ html: `Log Deleted` });
+  };
+
   return (
-    <li className="collection-item">
+    <li className='collection-item'>
       <div>
         <a
-          href="#edit-log-modal"
+          href='#edit-log-modal'
           className={`modal-trigger ${
             log.attention ? 'red-text' : 'blue-text'
           }`}
+          onClick={() => setCurrent(log)}
         >
           {log.message}
         </a>
         <br />
-        <span className="grey-text">
-          <span className="black-text">ID #{log.id}</span> last updated by{' '}
-          <span className="black-text">{log.tech}</span> on{' '}
-          <Moment format="YYYY/MM/DD, h:mm:ss a">{log.date}</Moment>
+        <span className='grey-text'>
+          <span className='black-text'>ID #{log.id}</span> last updated by{' '}
+          <span className='black-text'>{log.tech}</span> on{' '}
+          <Moment format='YYYY/MM/DD, h:mm:ss a'>{log.date}</Moment>
         </span>
-        <a href="#!" className="secondary-content">
-          <i className="material-icons grey-text">delete</i>
+        <a href='#!' className='secondary-content' onClick={onDelete}>
+          <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
     </li>
@@ -29,7 +39,12 @@ const LogItem = ({ log }) => {
 };
 
 LogItem.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.object.isRequired,
+  setCurrent: PropTypes.object.isRequired
 };
 
-export default LogItem;
+export default connect(
+  null,
+  { deleteLog, setCurrent }
+)(LogItem);

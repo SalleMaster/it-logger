de116c46b048b151bca44d4395a8750c5e9f305a
Sasa Radovanovic <salle90ar@gmail.com>
2019-09-27 10:43:19 +0200

    LogItem & Preloader

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useEffect, Fragment } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import 'materialize-css/dist/css/materialize.min.css';
+import Logs from './components/logs/Logs';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(() => {
+    // init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
